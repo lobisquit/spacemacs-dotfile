2316ccb99280d4579ad1900099672b8b586ca995
Enrico Lovisotto <enricolovisotto@gmail.com>
2018-02-18 10:47:48 +0100

    Added "local change directory" to eshell

--- eshell-conf.el
+++ eshell-conf.el
@@ -106,6 +106,18 @@
       (find-file file)
     (find-file ".")))
 
+;; local change directory (when using tramp)
+(defun eshell/lcd (&optional directory)
+  (if (file-remote-p default-directory)
+      (with-parsed-tramp-file-name default-directory nil
+        (eshell/cd (tramp-make-tramp-file-name
+                    (tramp-file-name-method v)
+                    (tramp-file-name-user v)
+                    (tramp-file-name-host v)
+                    (or directory "")
+                    (tramp-file-name-hop v))))
+    (eshell/cd directory)))
+
 (require 'cl)
 
 (defun argument-set-p (arg)
