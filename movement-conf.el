1069a502b7cf719d1c65be609fc07ee5bcb15ba3
Enrico Lovisotto <enricolovisotto@gmail.com>
2018-05-29 09:55:05 +0200

    Fixed smart line beginning
    Use (interactive "^") to make shift-select work

--- movement-conf.el
+++ movement-conf.el
@@ -1,9 +1,5 @@
 ;; -*- mode: emacs-lisp -*-
 
-;; move at actual beginning of the line (after indentation)
-;; (global-set-key (kbd "<home>") 'smart-line-beginning)
-;; TODO useful, but S-<home> gets ineffective!
-
 ;; switch to other window (faster than C-x o)
 (global-set-key (kbd "s-o") 'other-window)
 
@@ -11,10 +7,17 @@
 (global-set-key (kbd "C-c C-<left>") 'winner-undo)
 (global-set-key (kbd "C-c C-<right>") 'winner-redo)
 
+;; move at actual beginning of the line (after indentation)
+(global-set-key (kbd "<home>") 'smart-line-beginning)
+
 (defun smart-line-beginning ()
   "Jump to indentation or beginning of line if already there."
-  (interactive)
-  (let ((current-point (point)))
-    (beginning-of-line-text)
-    (when (eq current-point (point))
-      (beginning-of-line))))
+  (interactive "^")
+  ;; handle eshell differently
+  (if (bound-and-true-p eshell-mode)
+      (eshell-bol)
+    ;; normal behaviour
+    (let ((current-point (point)))
+      (beginning-of-line-text)
+      (when (eq current-point (point))
+        (beginning-of-line)))))
